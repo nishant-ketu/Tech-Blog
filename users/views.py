d41ee1e6261dac3c6699c9ee8d45aac90b2ce0b6
nishant-ketu <47020252+nishant-ketu@users.noreply.github.com>
2019-01-25 10:45:13 +0530

    error resolved

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shosublime rtcuts import render,redirect
+from django.shortcuts  import render,redirect
 from django.contrib import messages
 from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
